82c4f4c8f4517d5268ccf8e5f84714da3c331cf9
enbanned <nik.skvortsov2007@gmail.com>
2025-06-15 17:47:31 +0300

    updates

--- config.py
+++ config.py
@@ -1,5 +1,3 @@
-"""Configuration management for sticker hunter bot."""
-
 from dataclasses import dataclass
 
 
@@ -7,10 +5,10 @@ from dataclasses import dataclass
 class Config:
     
     # JWT токен из браузера
-    jwt_token: str = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJodHRwczovL2FwaS5zdGlja2VyZG9tLnN0b3JlIiwic3ViIjoiODkxODc5NTEyIiwiYXVkIjpbImh0dHBzOi8vc3RpY2tlcmRvbS5zdG9yZSJdLCJleHAiOjE3NDk5OTc1NzksIm5iZiI6MTc0OTk5Mzk3OSwiaWF0IjoxNzQ5OTkzOTc5LCJqdGkiOiI0MTQxYWViNS04ZTkxLTRkZTktYjFjNC1mYTMxMWRiY2IyY2MifQ.Y5qXI4-ZSj2flgNxdX-uEMsADJbMUS2tT7k7ywCcmSI"
+    jwt_token: str = "eyJhbGciOiJIUzI1N"
     
     # ton сид-фраза из 24 слов (например из TonKeeper)
-    ton_seed_phrase: str = "all index humble marine hurry select cross bid debate worth kitchen symptom retire diamond tunnel inch industry vague fold pony eagle portion evolve twelve"
+    ton_seed_phrase: str = "word1 word2 ... word24"
     
     stickers_per_purchase: int = 5  # Максимально количество за 1 order
     gas_amount: float = 0.1  # Газ для отправки транзакции
